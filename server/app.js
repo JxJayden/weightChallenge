c1dfab33a90064f622d4c93f530b916ead90609f
jayden <jxdjayden@gmail.com>
2017-12-02 20:46:29 +0800

    :heavy_plus_sign: remove koa-better-body & add koa-bodyparser

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 const Koa = require('koa')
-const logger = require('koa-logger')
+const koaLogger = require('koa-logger')
+const logger = require('./lib/log')
 const app = new Koa()
-const { router, api } = require('./routes')
+const router = require('./routes')
+const bodyparser = require('koa-bodyparser')
 
-app.use(logger())
-app.use(router.middleware())
-app.use(api.middleware())
+app.use(koaLogger())
+
+app.use(async (ctx, next) => {
+    try {
+        await next()
+    } catch (err) {
+        logger.error(err)
+        err.status = err.statusCode || err.status || 500
+        throw err
+    }
+})
+
+app.use(bodyparser()).use(router.middleware())
 
 module.exports = app
